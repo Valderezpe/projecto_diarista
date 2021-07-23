1a410cbfc5474f8250d21748e10223a1a4b11aae
valderez <valderezpe@gmail.com>
2021-07-22 21:04:25 -0300

    enviando primeira parte projecto

---
+++ src/ui/components/data-display/Footer/Footer.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  FooterStyled,
+  FooterContainer,
+  FooterTitle,
+  AppList,
+} from "./Footer.style";
+import { Typography, Box } from "@material-ui/core";
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      <FooterContainer>
+        <Box sx={{ maxWidth: "400px" }}>
+          <FooterTitle>Quem Somos</FooterTitle>
+          <Typography variant={"body2"} sx={{ mt: 2 }}>
+            E-Diarista te ajuda a encontrar um profissional perfeito para
+            realizar a limpeza dee sua casa, empresa.Garantimos a/o melhor
+            profissinal com total segurança e pratiiadade ! São milhares de
+            clientes satisfeito por todo pais..
+          </Typography>
+        </Box>
+
+        <div>
+          <FooterTitle>Baixe nossos aplicativos</FooterTitle>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_black"} rel={"noopenser moreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt={"App Store"} />
+              </a>
+            </li>
+
+            <li>
+              <a href={"/"} target={"_black"} rel={"noopenser moreferrer"}>
+                <img src={"/img/logos/google-play.png"} alt={"Google Play"} />
+              </a>
+            </li>
+          </AppList>
+        </div>
+      </FooterContainer>
+    </FooterStyled>
+  );
+};
+
+export default Footer;
